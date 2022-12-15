ff6688383f150b2c779201ae1cc4209265ad8797
Aiko Ramalho <aiko.ramalho@gmail.com>
2022-12-15 16:04:53 -0300

    refactor(auth): modify error-handler middleware to get error from

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -5,17 +5,11 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
   console.log('something went wrong');
 
   if (err instanceof RequestValidationError) {
-    const formattedErrors = err.errors.map(err => {
-      return {
-        message: err.msg,
-        field: err.param
-      }
-    })
-    return res.status(400).send({errors: formattedErrors})
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() })
   }
 
   if (err instanceof DatabaseConnectionError) {
-    return res.status(500).send({ errors: [{ message: err.reason }] })
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() })
   }
 
   res.send(400).send({
