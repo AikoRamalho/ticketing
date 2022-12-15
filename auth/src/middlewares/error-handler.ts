a7728c7a7f49042020ae950c6b4922331caa07da
Aiko Ramalho <aiko.ramalho@gmail.com>
2022-12-15 17:04:51 -0300

    refactor(auth): modify error-handler to check if error is instanceof abstract class CustomError

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,15 +1,10 @@
 import { Request, Response, NextFunction } from 'express'
-import { DatabaseConnectionError } from '../errors/database-connection-error';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { CustomError } from '../errors/custom-error';
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   console.log('something went wrong');
 
-  if (err instanceof RequestValidationError) {
-    return res.status(err.statusCode).send({ errors: err.serializeErrors() })
-  }
-
-  if (err instanceof DatabaseConnectionError) {
-    return res.status(err.statusCode).send({ errors: err.serializeErrors() })
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   res.send(400).send({
