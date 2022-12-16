3c3333564009d2b583e0d9945c81c42f7cdf3559
Aiko Ramalho <aiko.ramalho@gmail.com>
2022-12-15 23:11:11 -0300

    feat(auth): create mongodb connection

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 import { currentUserRouter } from './routes/current-user';
 import { signupRouter } from './routes/signup';
 import { signinRouter } from './routes/signin';
@@ -22,6 +23,16 @@ app.all('*', () => {
 
 app.use(errorHandler)
 
-app.listen(3000, ()=>{
-  console.log('listen on 3000!');
-})
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('connected to mongodb');
+  } catch (err) {
+    console.log(err);
+  }
+  app.listen(3000, ()=>{
+    console.log('listen on 3000!');
+  })
+}
+
+start();
